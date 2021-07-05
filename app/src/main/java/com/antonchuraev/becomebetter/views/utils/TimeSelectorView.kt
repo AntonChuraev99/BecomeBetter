e66990e6dbda055f08ef5b6722eb7fc6ddea9333
AntonChuraev99 <churaevanton@gmail.com>
2021-07-05 17:24:24 +0300

    add goal

--- app/src/main/java/com/antonchuraev/becomebetter/views/utils/TimeSelectorView.kt
+++ app/src/main/java/com/antonchuraev/becomebetter/views/utils/TimeSelectorView.kt
@@ -3,17 +3,32 @@ package com.antonchuraev.becomebetter.views.utils
 import android.content.Context
 import android.util.AttributeSet
 import com.antonchuraev.becomebetter.R
-import com.antonchuraev.becomebetter.dataClasses.Goal
-import com.antonchuraev.becomebetter.databinding.ViewGoalInListBinding
-import com.antonchuraev.becomebetter.helpers.extensions.toast
+import com.antonchuraev.becomebetter.databinding.ViewTimeSelectorBinding
 import com.antonchuraev.becomebetter.views.CustomView
+import java.util.*
 
 
-class TimeSelectorView @JvmOverloads constructor(context: Context , attrs: AttributeSet? = null , defStyleAttr: Int = 0 ): CustomView<ViewGoalInListBinding>(context, attrs, defStyleAttr) {
+class TimeSelectorView @JvmOverloads constructor(context: Context , attrs: AttributeSet? = null , defStyleAttr: Int = 0 ): CustomView<ViewTimeSelectorBinding>(context, attrs, defStyleAttr) {
 
     override fun getLayoutRes() = R.layout.view_time_selector
 
 
+    init {
 
+        binding.slider.setLabelFormatter{
+            generateTextForSlider(it).apply { binding.selectedSize.text = this }
+        }
+    }
 
+    private fun generateTextForSlider(sliderValue: Float): String {
+         return when (sliderValue){
+             0F->context.getString(R.string.forever)
+             1F->context.getString(R.string.one_day)
+             2F->context.getString(R.string.three_days)
+             3F->context.getString(R.string.one_week)
+             4F->context.getString(R.string.one_month)
+             5F->context.getString(R.string.one_year)
+             else -> throw Exception("Значение не определено")
+         }
+    }
 }
