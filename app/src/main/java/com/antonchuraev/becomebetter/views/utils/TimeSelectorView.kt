9517c2f8a431436898c9530443ad6d005d50a230
AntonChuraev99 <churaevanton@gmail.com>
2021-07-07 18:05:33 +0300

    create goal

--- app/src/main/java/com/antonchuraev/becomebetter/views/utils/TimeSelectorView.kt
+++ app/src/main/java/com/antonchuraev/becomebetter/views/utils/TimeSelectorView.kt
@@ -8,27 +8,60 @@ import com.antonchuraev.becomebetter.views.CustomView
 import java.util.*
 
 
-class TimeSelectorView @JvmOverloads constructor(context: Context , attrs: AttributeSet? = null , defStyleAttr: Int = 0 ): CustomView<ViewTimeSelectorBinding>(context, attrs, defStyleAttr) {
+class TimeSelectorView @JvmOverloads constructor(
+		context: Context , attrs: AttributeSet? = null , defStyleAttr: Int = 0) :
+		CustomView<ViewTimeSelectorBinding>(context , attrs , defStyleAttr) {
 
-    override fun getLayoutRes() = R.layout.view_time_selector
+	override fun getLayoutRes() = R.layout.view_time_selector
+
+	private enum class Type(val referenceToAttr: Int , val startFrom:Int,val values: Map<Float , Int>) {
+		GOAL_DURATION(
+			0 , 0, mapOf(
+				0F to R.string.forever , 1F to R.string.one_day , 2F to R.string.three_days ,
+				3F to R.string.one_week , 4F to R.string.one_month , 5F to R.string.one_year
+					 )
+					 ) ,
+		GOAL_PRIORITY(
+			1 , 1, mapOf(
+				0F to R.string.small_prority , 1F to R.string.standart_prority ,
+				2F to R.string.higherst_prority
+					 )
+					 );
+
+		companion object {
+			fun findByAttrs(attrReference: Int) =
+				values().find { it.referenceToAttr == attrReference } ?: throw java.lang.Exception(
+					"нету такого стиля"
+																								  )
+		}
+
+	}
 
 
-    init {
+	init {
+        var style = Type.GOAL_DURATION
 
-        binding.slider.setLabelFormatter{
-            generateTextForSlider(it).apply { binding.selectedSize.text = this }
-        }
-    }
+         context.theme.obtainStyledAttributes(
+			attrs , R.styleable.TimeSelectorView , 0 , 0
+                                                        ).apply {
+			try {
+                style = Type.findByAttrs(getInteger(R.styleable.TimeSelectorView_type , 0))
+			} finally {
+				recycle()
+			}
+		}
 
-    private fun generateTextForSlider(sliderValue: Float): String {
-         return when (sliderValue){
-             0F->context.getString(R.string.forever)
-             1F->context.getString(R.string.one_day)
-             2F->context.getString(R.string.three_days)
-             3F->context.getString(R.string.one_week)
-             4F->context.getString(R.string.one_month)
-             5F->context.getString(R.string.one_year)
-             else -> throw Exception("Значение не определено")
-         }
-    }
+		binding.slider.apply {
+			valueTo = style.values.size.toFloat() - 1F
+			value = style.startFrom.toFloat()
+			binding.selectedSize.text = generateTextForSlider(style.referenceToAttr , value)
+			setLabelFormatter {
+				generateTextForSlider(style.referenceToAttr , it).apply { binding.selectedSize.text = this }
+			}
+		}
+	}
+
+	private fun generateTextForSlider(styleValue: Int , sliderValue: Float): String {
+		return context.getString(Type.findByAttrs(styleValue).values[sliderValue]!!)
+	}
 }
