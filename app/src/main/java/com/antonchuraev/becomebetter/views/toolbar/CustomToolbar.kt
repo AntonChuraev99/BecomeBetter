37bd8f3f8d967ea17a36616a80ce7551175a7b20
AntonChuraev <churaevanton@gmail.com>
2021-08-10 17:44:04 +0300

    create mode

--- app/src/main/java/com/antonchuraev/becomebetter/views/toolbar/CustomToolbar.kt
+++ app/src/main/java/com/antonchuraev/becomebetter/views/toolbar/CustomToolbar.kt
@@ -8,6 +8,7 @@ import androidx.core.view.isVisible
 import com.antonchuraev.becomebetter.R
 import com.antonchuraev.becomebetter.base.BasePresenter
 import com.antonchuraev.becomebetter.databinding.ViewCustomToolbarBinding
+import com.antonchuraev.becomebetter.helpers.extensions.gone
 import com.antonchuraev.becomebetter.helpers.extensions.show
 import com.antonchuraev.becomebetter.views.CustomView
 import com.historic.app.global.navigation.FlowRouter
@@ -17,7 +18,14 @@ import ru.terrakok.cicerone.Router
 class CustomToolbar @JvmOverloads constructor(context: Context , attrs: AttributeSet? = null , defStyleAttr: Int = 0) : CustomView<ViewCustomToolbarBinding>(context , attrs , defStyleAttr) {
 
 	fun show() {
+		hideAllViews()
 		this.isVisible = true
+	}
+
+	private fun hideAllViews() {
+		binding.leftImage.gone()
+		binding.rigthtImage.gone()
+		binding.tittle.gone()
 	}
 
 	fun defaultBackButton(presenter: Router?) {
