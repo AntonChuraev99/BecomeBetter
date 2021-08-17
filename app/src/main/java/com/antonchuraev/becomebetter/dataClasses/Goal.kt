ce307d86d0bf2ec0bd76f5614dea9d6af43c10a4
AntonChuraev <churaevanton@gmail.com>
2021-08-17 17:55:39 +0300

    goal progress type

--- app/src/main/java/com/antonchuraev/becomebetter/dataClasses/Goal.kt
+++ app/src/main/java/com/antonchuraev/becomebetter/dataClasses/Goal.kt
@@ -4,22 +4,34 @@ import java.io.Serializable
 import java.time.Duration
 
 data class Goal(
-        val name:String,
-        /**
-         * 0F to R.string.forever , 1F to R.string.one_day , 2F to R.string.three_days ,
-           3F to R.string.one_week , 4F to R.string.one_month , 5F to R.string.one_year
-         */
-        val duration:Float = 0F,
-        val description:String? = null,
-        /**
-         * 0F to R.string.small_prority , 1F to R.string.standart_prority ,
-                2F to R.string.higherst_prority
-         */
-        val priority:Float = 1F,
-        var progress:Int = 0 ,//прогресс от  0%=0 до 100%=100
-        var isSelected:Boolean = false //для адаптера
-):Serializable {
+    val name: String,
 
+    /**
+     * 0F to R.string.forever , 1F to R.string.one_day , 2F to R.string.three_days ,
+    3F to R.string.one_week , 4F to R.string.one_month , 5F to R.string.one_year
+     */
+    val duration: Float = 0F,
+    val description: String? = null,
 
+    /**
+     * 0F to R.string.small_prority , 1F to R.string.standart_prority ,
+    2F to R.string.higherst_prority
+     */
+    val priority: Float = 1F,
+    var progress: Int = 0,//прогресс от  0%=0 до 100%=100
+    var isSelected: Boolean = false,//для адаптера выбранно ли
+    var progressType: ProgressType = ProgressType.PERCENTS //тип как ведется проогресс
+
+) : Serializable {
+
+    /**
+     * тип как ведется прогресс
+     * есть проценты и дни
+     */
+    enum class ProgressType() {
+        PERCENTS,
+        DAYS,
+
+    }
 
 }
