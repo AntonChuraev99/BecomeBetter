0a8db0ff850f5ef6bf1c660bf3894d770ab869aa
AntonChuraev <churaevanton@gmail.com>
2021-08-18 12:38:10 +0300

    create goals views

--- app/src/main/java/com/antonchuraev/becomebetter/fragments/bottomNavigation/addGoal/createGoalFromTemplate/CreateGoalFromTemplatesPresenter.kt
+++ app/src/main/java/com/antonchuraev/becomebetter/fragments/bottomNavigation/addGoal/createGoalFromTemplate/CreateGoalFromTemplatesPresenter.kt
@@ -10,11 +10,12 @@ class CreateGoalFromTemplatesPresenter: BasePresenter<CreateGoalFromTemplatesVie
     fun loadTemplates(){
         val templates = mutableListOf<Goal>()
 
-        templates.add(Goal("Бросить курить" , duration = 4F , "Бросить курить за месяц", priority = 2F))
-        templates.add(Goal("Начать регулярно бегать" , duration = 0F , "Регулярно бегать", priority = 1F))
-        templates.add(Goal("Ежедневно читать минимум по 30 минут" , duration = 0F , null , priority = 1F))
-        templates.add(Goal("Кушать здоровую пищу в течение года" , duration = 5F , null, priority = 1F))
+        templates.add(Goal("Не курить 30 дней" , duration = 4F , "Бросить курить за месяц", priority = 2F , progressType = Goal.ProgressType.DAYS))
+        templates.add(Goal("Начать регулярно бегать" , duration = 0F , "Регулярно бегать", priority = 1F , progressType = Goal.ProgressType.DAYS))
+        templates.add(Goal("Ежедневно читать минимум по 30 минут" , duration = 0F , null , priority = 1F , progressType = Goal.ProgressType.DAYS))
+        templates.add(Goal("Кушать здоровую пищу в течение года" , duration = 5F , null, priority = 1F , progressType = Goal.ProgressType.DAYS))
         templates.add(Goal("Похудеть" , duration = 0F , "Похудеть на [введите кол-во кг]", priority = 2F))
+        templates.add(Goal("Накопить 100 тысяч" , duration = 0F ,  priority = 2F))
 
         viewState.showTemplates(templates)
     }
